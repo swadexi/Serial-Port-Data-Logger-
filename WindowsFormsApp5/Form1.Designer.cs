8a7091cc69d0a8f8e6b5f9a7a25862c583cee6ed
Swadexi Istiqphara <swadexi@gmail.com>
2020-06-01 04:20:53 +0700

    Add files via upload

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SP = new System.IO.Ports.SerialPort(this.components);
+            this.cbPort = new System.Windows.Forms.ComboBox();
+            this.btnRescan = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.txtRecordFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.saveDialog = new System.Windows.Forms.SaveFileDialog();
+            this.dataLabel = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.baudRate = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(23, 82);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // SP
+            // 
+            this.SP.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.Serial_DataReceived);
+            // 
+            // cbPort
+            // 
+            this.cbPort.FormattingEnabled = true;
+            this.cbPort.Location = new System.Drawing.Point(23, 25);
+            this.cbPort.Name = "cbPort";
+            this.cbPort.Size = new System.Drawing.Size(177, 21);
+            this.cbPort.TabIndex = 1;
+            // 
+            // btnRescan
+            // 
+            this.btnRescan.Location = new System.Drawing.Point(125, 82);
+            this.btnRescan.Name = "btnRescan";
+            this.btnRescan.Size = new System.Drawing.Size(75, 23);
+            this.btnRescan.TabIndex = 2;
+            this.btnRescan.Text = "Rescan";
+            this.btnRescan.UseVisualStyleBackColor = true;
+            this.btnRescan.Click += new System.EventHandler(this.btnRescan_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.LineWidth = 3;
+            chartArea1.AxisY.LineWidth = 3;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 117);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1061, 300);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart";
+            // 
+            // txtRecordFile
+            // 
+            this.txtRecordFile.Location = new System.Drawing.Point(255, 25);
+            this.txtRecordFile.Name = "txtRecordFile";
+            this.txtRecordFile.Size = new System.Drawing.Size(194, 20);
+            this.txtRecordFile.TabIndex = 4;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(456, 25);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(53, 23);
+            this.btnBrowse.TabIndex = 5;
+            this.btnBrowse.Text = "Save";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // dataLabel
+            // 
+            this.dataLabel.Location = new System.Drawing.Point(255, 52);
+            this.dataLabel.Name = "dataLabel";
+            this.dataLabel.Size = new System.Drawing.Size(194, 20);
+            this.dataLabel.TabIndex = 6;
+            this.dataLabel.Text = "Label";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(456, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Data Label";
+            // 
+            // baudRate
+            // 
+            this.baudRate.FormattingEnabled = true;
+            this.baudRate.Items.AddRange(new object[] {
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.baudRate.Location = new System.Drawing.Point(23, 55);
+            this.baudRate.Name = "baudRate";
+            this.baudRate.Size = new System.Drawing.Size(177, 21);
+            this.baudRate.TabIndex = 8;
+            this.baudRate.Text = "9600";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1085, 429);
+            this.Controls.Add(this.baudRate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataLabel);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtRecordFile);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btnRescan);
+            this.Controls.Add(this.cbPort);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Arduino Analog Data Logger";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.IO.Ports.SerialPort SP;
+        private System.Windows.Forms.ComboBox cbPort;
+        private System.Windows.Forms.Button btnRescan;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox txtRecordFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.SaveFileDialog saveDialog;
+        private System.Windows.Forms.TextBox dataLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox baudRate;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        Boolean isRecording;
+        String RxString;
+        public Form1()
+        {
+            InitializeComponent();
+            GetSerialDevice();
+        }
+        SerialPort temperature = new SerialPort();
+        public void GetSerialDevice()
+        {
+
+            // Get a list of serial port names.        
+            String[] ports = SerialPort.GetPortNames();
+            foreach (String portT in ports) { cbPort.Items.Add(portT); }
+            cbPort.SelectedIndex = cbPort.Items.Count - 1;
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (btnConnect.Text == "Connect")
+            {
+                try
+                {
+                    String rx = cbPort.Text;
+                    //Sets up serial port
+                    SP.PortName = rx;
+                    SP.BaudRate = 9600;
+                    SP.Handshake = System.IO.Ports.Handshake.None;
+                    SP.DataBits = 8;
+                    SP.ReadTimeout = 200;
+                    SP.WriteTimeout = 50;
+                    SP.ReceivedBytesThreshold = 2;
+                    SP.Open();
+
+                    //Sets button State and Creates function call on data recieved
+                    btnConnect.Text = "Disconnect";
+                 }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            else
+            {
+                try // just in case serial port is not open could also be acheved using if(serial.IsOpen)
+                {
+                    SP.Close();
+                    btnConnect.Text = "Connect";
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            
+            saveDialog.Filter = "CSV Files (*.csv)| *.csv";
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtRecordFile.Text = saveDialog.FileName;
+                isRecording = true;
+            }
+            
+        }
+
+        private void Serial_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                RxString = "";
+               // SP.Encoding = Encoding.GetEncoding(28591);
+                RxString = SP.ReadLine();
+                this.Invoke(new EventHandler(Record));
+            }
+            catch {; }        
+        }
+        int dataLogCounter = 0;
+        private void Record(object sender, EventArgs e)
+        {
+                if (isRecording == true)
+                {
+                    dataLogCounter++;
+                    if (dataLogCounter == 1)
+                    {
+                        System.IO.File.WriteAllText(saveDialog.FileName, RxString);
+                    }                    
+                    else
+                    {
+                        using (System.IO.StreamWriter file = new System.IO.StreamWriter(saveDialog.FileName, true))
+                        {                            
+                            file.Write(DateTime.Now.ToString() + ',' + dataLabel.Text + ',' +   RxString  );
+                        }
+                    }
+                }
+
+                ParseData(RxString);
+        }
+        bool hasBeenAdd=false;
+        int num_data;
+        private void ParseData(String str)
+        {
+            try { 
+            string[] data = str.Split(' ');
+            num_data = data.Count();
+                chart1.Series[0].BorderWidth = 2;
+                chart1.Series[0].Points.Add(Convert.ToDouble(data[0]));            
+            if (chart1.Series[0].Points.Count > 300)
+            {
+                chart1.Series[0].Points.RemoveAt(0);
+
+            }
+
+            if(num_data>1 && hasBeenAdd == false) { 
+                for(int i = 1; i <= num_data; i++) { 
+                chart1.Series.Add(i.ToString());
+                        chart1.Series[i.ToString()].BorderWidth = 2;
+                    }
+            hasBeenAdd = true;
+            }
+            if (num_data > 1)
+            {
+                for (int i = 1; i <= num_data; i++)
+                {
+                    chart1.Series[i].Points.Add(Convert.ToDouble(data[i]));
+                    if (chart1.Series[i].Points.Count > 20)
+                    {
+                        chart1.Series[i].Points.RemoveAt(0);
+                    }
+                }
+            }
+            }
+            catch
+            {
+                ;
+            }
+
+        }
+
+        private void btnRescan_Click(object sender, EventArgs e)
+        {
+            GetSerialDevice();
+        }
+    }
+
+}
